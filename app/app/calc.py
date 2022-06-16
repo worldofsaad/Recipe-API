ef850401643cc5d5ae13bbfdea59bb34fa0fd382
Saad <worldofsaad@gmail.com>
2022-06-16 13:01:26 +0200

    Added PostGres Adapter

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """ Add x and y and return results. """
+    return x + y
+
+def subtract(x, y):
+    """ Add x from y and return results. """
+    return y - x
